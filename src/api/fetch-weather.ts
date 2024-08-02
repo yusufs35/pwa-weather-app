8f25938c212211f460e78a75497905c107ab7737
Scott <yusufs3435@gmail.com>
2024-08-02 19:07:10 +0200

    v2

--- src/api/fetch-weather.ts
+++ src/api/fetch-weather.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const URL = "https://api.openweathermap.org/data/2.5/weather";
-const API_KEY = "716196ecf8dec642593121385a539ddd";
+const API_KEY = import.meta.env.VITE_OPEN_WEATHER_MAP_API_KEY;
 
 export const fetchWeather = async (query: string) => {
+	if (API_KEY === "your_key") alert("Type your weather api key in env file");
+
 	const data = await axios.get(URL, {
 		params: {
 			q: query,
